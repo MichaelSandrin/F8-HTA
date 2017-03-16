19d0749228613fa0794dc300889549ab5d3bfab4
RileySchina <riley.schina@gmail.com>
2017-03-15 19:07:47 -0700

    glow

---
+++ Prototype/Assets/Scripts/GlowWing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowWing : MonoBehaviour {
+
+    float glideColor;
+	// Use this for initialization
+	void Start () {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        glideColor = GameObject.Find("Plume").GetComponent<CharacterMovement>().glideLockoutTimer;
+
+        Material mat = renderer.material;
+
+        //float emission = Mathf.PingPong(Time.time, 1.2f);
+        Color baseColor = Color.yellow; //Replace this with whatever you want for your base color at emission level '1'
+
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(glideColor);
+
+        mat.SetColor("_EmissionColor", finalColor);
+    }
+
+    void OnGUI()
+    {
+        string glideTime = glideColor.ToString();
+        GUI.Label(new Rect(0, 0, 100, 50), glideTime);
+    }
+}
