ec782f9736d9f72c3b4cac4f49da5b2d0e69f686
RileySchina <riley.schina@gmail.com>
2017-03-18 13:38:39 -0700

    putting glow back in

---
+++ Prototype/Assets/Scripts/GlowWing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowWing : MonoBehaviour {
+
+    float glideColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        glideColor = GameObject.Find("Plume").GetComponent<CharacterMovement>().glideEndurance;
+        Renderer renderer = GetComponent<Renderer>();
+        Material mat = renderer.material;
+
+       // float emission = Mathf.PingPong(Time.time, 1.0f);
+        Color baseColor = Color.yellow; //Replace this with whatever you want for your base color at emission level '1'
+
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(glideColor);
+
+        mat.SetColor("_EmissionColor", finalColor);
+
+        /*Settings for the glow, main Camera:
+         Rendering Path: Deferred
+         HDR should be on
+         Add Bloom to main Camera:
+         Intensity: 1.5
+         Threshold: 1
+         Blur Iterations: 1
+         Sample Distance: .5
+         */
+    }
+}
